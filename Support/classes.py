f21cd68b69df1dfd34b05f7db24ea5dff67a76ba
João Amgarten <joaoamgarten2003@gmail.com>
2022-08-13 00:40:26 +0100

    build: create login option in main.py

--- Support/classes.py
+++ Support/classes.py
@@ -172,8 +172,8 @@ class Email:
     @staticmethod
     def send_email(receiver, token):
         """Send an email with the authentication token"""
-        email_sender = 'your_email_here@gmail.com'  # Your Google account here
-        email_password = 'your_app_password_here'  # Your app password here
+        email_sender = 'rotcivoaoj1@gmail.com'  # Your Google account here
+        email_password = 'vrredstvxxjvppzn'  # Your app password here
         email_receiver = receiver
 
         msg = EmailMessage()
