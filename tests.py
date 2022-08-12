5ca5fc88b2d62efea3b8fb2b063dab83c219a812
João Amgarten <joaoamgarten2003@gmail.com>
2022-08-12 19:14:01 +0100

    build: test all the method and attributes

--- tests.py
+++ tests.py
@@ -2,10 +2,34 @@ from Support import classes
 
 # Creating the connection to the database
 database = classes.ConnectDB('localhost', 'root', '', 'login_python')
-
-print(database.connect())
+database.connect()
 
 token_obj = classes.Token()
 token = token_obj.generate_token()
 
-print(token)
+acc = classes.Account('joao', 'joao@gmail.com', '87312')
+
+
+# Testing Attributes
+print('======== TESTING ATTRIBUTES ========')
+print(f"host attr: {database.host}")
+print(f"user attr: {database.user}")
+print(f"name attr: {database.name}")
+print(f"user attr: {acc.user}")
+print(f"email attr: {acc.email}")
+
+# Testing Methods
+print('\n======== TESTING METHODS ========')
+print(f"ConnectDB __repr__: {database}")
+print(f"return_infos(account): {database.return_infos('accounts')}\n")
+print(f"return_infos(tokens): {database.return_infos('tokens')}\n")
+print(f"check_user(amgarten): {database.check_user('amgarten')}\n")
+print(f"check_email(amgarten@gmail.com): {database.check_email('amgarten@gmail.com')}\n")
+# print(f"send_account: {database.send_account(('Teste', 'teste@gmail.com', '231'))}")
+print(f"Token __repr__: {token_obj}\n")
+print(f"generate_token: {token}\n")
+print(f"send_token(token): {database.send_token(token)}\n")
+print(f"Account __repr__: {acc}\n")
+print(f"{database.send_account(acc.user, acc.email, acc.password)}\n")
+print(f"activate_token(token): {token_obj.activate_token('1e144')}\n")
+print(database.check_account('testaccount@gmail.com', '2005'))
